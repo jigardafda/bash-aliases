5e3f52b40a17981f7f56de452b632dbbe9dc319e
Jigar Dafda <jigar.dafda@gmail.com>
2014-12-10 15:12:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 wget -O ~/.aliases.sh --no-check-certificate https://raw.githubusercontent.com/jigardafda/bash-aliases/master/aliases.sh
 echo 'source ~/.aliases.sh' >> ~/.bashrc
 source ~/.bashrc
+echo 'Bash-Aliases installed successfully. :)'
