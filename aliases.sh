733599e74578cf4875b311fdbf673b7fa7f41a79
Jigar Dafda <jigar.dafda@gmail.com>
2014-12-10 12:01:16 +0530

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Author: Jigar Dafda (jigar.dafda@gmail.com)
+# Bash Aliases 
+ 
+# RELOAD BASHRC
+alias reload='. ~/.bashrc'
+
+# GENERAL
+alias l='ls -lah'
+alias c='clear'
+alias o='open .'
+alias tf='tail -f -n 100'
+alias httpdump='sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*"'
+
+# SUBLIME
+alias e='subl' 
+alias ec='subl . &'
+
+# NAVIGATION AND LISTING 
+alias '..'='cd ..'
+alias cd..='cd ..'
+alias sl='ls'
+alias ll='ls -la'
+
+# GIT
+alias gb='git branch'
+alias gl='git log'
+alias glo='git log --oneline'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gcam='git commit --amend'
+alias gd='git diff'
+alias gp='git pull'
+alias gpr='git pull --rebase'
+alias gpu='git push'
+alias gst='git status'
+alias gss='git stash save'
+alias gsa='git stash apply'
+alias gsp='git stash pop'
+alias gsl='git stash list'
+alias grc='git rebase --continue'
+alias gri='git rebase --interactive'
+alias gpuh='git push origin master'
+
+# NODEJS AND NPM
+alias ninit='npm init'
+alias npmi='npm install'
+alias npmis='npm install --save'
+alias npmisd='npm install --save-dev'
+alias npms='npm start'
+alias npmu='npm update'
+alias npmt='npm test'
+
+# FOREVER NODEJS
+alias frvr='forever'
+alias frs='forever start'
+alias fst='forever stop'
+alias fsta='forever stopall'
+alais flist='forever list'
+
+# MONGO
+alias mstart='sudo mongod --dbpath /data/db'
